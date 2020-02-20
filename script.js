e2bc9932d11bc28059683c7e5e7b4783f6e06f6e
Andrei-Mihutoni <anfrei.mihutoni@gmail.com>
2020-02-20 11:38:20 +0100

    building the html "resort card";   js: connecting the sheets and fetching the data

---
+++ script.js
@@ -0,0 +1,51 @@
+//conecting the link to the google sheets data
+const link = "https://spreadsheets.google.com/feeds/list/1FSy3AueANJmJWDjB3LnMtNISPU4uvWdPvdfRWyeHJAk/od6/public/values?alt=json";
+window.addEventListener("DOMContentLoaded", getData);
+
+//fetch the Json data
+function getData() {
+    fetch(link)
+        .then(res => res.json())
+        .then(handleData);
+}
+
+function handleData(data) {
+
+    const myData = data.feed.entry;
+
+    console.log("myData - console:");
+    console.log(myData)
+    myData.forEach(showData)
+}
+
+function showData(singleRowData) {
+    console.log("single Row Data - console");
+    console.log(singleRowData.gsx$country.$t);
+    addResort(singleRowData);
+
+}
+;
+
+
+function addResort(data) {
+
+    //grab the template
+    const template = document.querySelector("template").content;
+
+
+    //make a copy
+    const clone = template.cloneNode(true);
+    console.log("here");
+
+    //change the content
+    clone.querySelector(".country span").textContent = data.gsx$country.$t;
+    clone.querySelector(".resort").textContent = data.gsx$resort.$t;
+    clone.querySelector(".max_height").textContent = data.gsx$maxheight.$t;
+    clone.querySelector(".slope_lenght").textContent = data.gsx$slopelength.$t;
+
+
+
+    //    append the clone
+    document.querySelector("main").appendChild(clone);
+
+}
